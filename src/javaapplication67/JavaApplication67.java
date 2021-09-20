7c54175b89c955e808c09d9dd614872f57c210d2
Juanjo <43281331+babolat22@users.noreply.github.com>
2021-09-20 09:59:31 -0300

    Version 1.0 HELLO GIT NETBEANS++

--- src/javaapplication67/JavaApplication67.java
+++ src/javaapplication67/JavaApplication67.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication67;
 
 /**
@@ -15,8 +11,11 @@ public class JavaApplication67 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+  
         System.out.println("Hola GIT");
+        
+        System.out.println("Comision 2");
+        
     }
     
 }
